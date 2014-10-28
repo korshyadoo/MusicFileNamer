b88df536a64e7ebd92577ace13096e5ee2238288
Jason Matthews <jmatthews@lixar.com>
2014-10-28 15:30:12 -0300

    Added default folder to config

--- src/main/java/com/korshyadoo/musicFileNamer/conf/Configuration.java
+++ src/main/java/com/korshyadoo/musicFileNamer/conf/Configuration.java
@@ -1,8 +1,11 @@
 package com.korshyadoo.musicFileNamer.conf;
 
+import java.io.File;
+
 public interface Configuration {
 	public int getMainWindowX();
 	public int getMainWindowY();
 	public int getMainWindowWidth();
 	public int getMainWindowHeight();
+	public File getDefaultDirectory();
 }
--- src/main/java/com/korshyadoo/musicFileNamer/conf/DevConfiguration.java
+++ src/main/java/com/korshyadoo/musicFileNamer/conf/DevConfiguration.java
@@ -1,5 +1,7 @@
 package com.korshyadoo.musicFileNamer.conf;
 
+import java.io.File;
+
 public class DevConfiguration implements Configuration {
 	public int getMainWindowX() {
 		return 500;
@@ -16,5 +18,12 @@ public class DevConfiguration implements Configuration {
 	public int getMainWindowHeight() {
 		return 600;
 	}
+
+	@Override
+	public File getDefaultDirectory() {
+		File file = new File("C:\\Users\\jmatthews\\Google Drive\\MusicFileNamer\\testFolder");
+		return file;
+	}
+	
 	
 }
